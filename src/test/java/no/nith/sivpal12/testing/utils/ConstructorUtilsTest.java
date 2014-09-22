d1c765e271bcb8e153f933de456db0eb3fed55c1
Pal M Sivertsen <palmarius@hotmail.com>
2014-09-22 14:31:06 +0200

    Added constructor utils for testing accessmodifier and adding line coverage

---
+++ src/test/java/no/nith/sivpal12/testing/utils/ConstructorUtilsTest.java
@@ -0,0 +1,29 @@
+package no.nith.sivpal12.testing.utils;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ConstructorUtilsTest {
+
+    @Test
+    public void isPrivate_ConstructorIsPrivate_ReturnsTrue() throws Exception {
+        assertTrue(ConstructorUtils.isPrivate(PrivateConstructor.class));
+    }
+
+    @Test
+    public void isPrivate_ConstructorIsPublic_ReturnsFalse() throws Exception {
+        assertFalse(ConstructorUtils.isPrivate(PublicConstructor.class));
+    }
+
+    public static class PublicConstructor {
+    }
+
+    public static class PrivateConstructor {
+        private PrivateConstructor() {
+            // Blank
+        }
+    }
+
+}
